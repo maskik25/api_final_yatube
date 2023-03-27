4d2e813ca833b955b370f65c041d02012293abcb
Максим Решетников <battlemaskik@yandex.ru>
2023-03-27 19:20:37 +0300

    sprint 9 rev 1

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,7 +8,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.method in permissions.SAFE_METHODS or
-            request.user and
-            request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
         )
